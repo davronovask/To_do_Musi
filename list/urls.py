27f52ddd8e4c9efadf567de6ecebbab4e8d94efc
davronovask <166344424+davronovask@users.noreply.github.com>
2025-05-13 10:05:47 +0600

    Сделала кнопку изменения задачи

--- list/urls.py
+++ list/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('edit/<int:task_id>/', views.update_task, name='edit_task'),
 ]
--- list/views.py
+++ list/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
 from .models import Task
 from .forms import TaskForm
 
@@ -32,4 +34,14 @@ def complete_task(request, task_id):
 def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id, user=request.user)
     task.delete()
+    return redirect('home')
+
+@require_POST
+@login_required
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    new_title = request.POST.get('title')
+    if new_title:
+        task.title = new_title
+        task.save()
     return redirect('home')
