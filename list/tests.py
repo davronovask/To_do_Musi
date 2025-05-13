c162fb84b244ffb1acc095ab356a4e09848d6a4a
davronovask <166344424+davronovask@users.noreply.github.com>
2025-05-13 11:45:00 +0600

    Добавила тесты

--- list/tests.py
+++ list/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from list.models import Task
 
-# Create your tests here.
+class HomePageTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='user', password='12345')
+        self.client.login(username='user', password='12345')
+        self.url = reverse('home')
+
+    def test_home_status_code(self):
+        """Проверяет, что страница /home/ открывается (код 200)"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_template_used(self):
+        """Проверяет, что используется шаблон home.html"""
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_user_name_visible(self):
+        """Имя пользователя отображается в боковой панели"""
+        response = self.client.get(self.url)
+        self.assertContains(response, self.user.username)
+
+    def test_create_new_task(self):
+        """Создание новой задачи через форму на главной"""
+        response = self.client.post(self.url, {'title': 'New Task'})
+        self.assertEqual(Task.objects.count(), 1)
+        task = Task.objects.first()
+        self.assertEqual(task.title, 'New Task')
+        self.assertEqual(task.user, self.user)
+        self.assertRedirects(response, self.url)
+
+    def test_display_active_and_completed_tasks(self):
+        """Задачи делятся на активные и завершённые"""
+        Task.objects.create(title='Active Task', user=self.user, completed=False)
+        Task.objects.create(title='Done Task', user=self.user, completed=True)
+
+        response = self.client.get(self.url)
+
+        self.assertContains(response, 'Active Task')
+        self.assertContains(response, 'Done Task')
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserAuthTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
+        self.logout_url = reverse('logout')
+        self.home_url = reverse('home')
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'testpass123'
+        }
+
+    def test_register_user_success(self):
+        response = self.client.post(self.register_url, {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'username': self.credentials['username'],
+            'password1': self.credentials['password'],
+            'password2': self.credentials['password'],
+        })
+        self.assertRedirects(response, self.home_url)
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+    def test_register_user_password_mismatch(self):
+        response = self.client.post(self.register_url, {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'username': 'wrongpass',
+            'password1': 'pass1234',
+            'password2': 'pass12345',
+        })
+        self.assertContains(response, "The two password fields didn’t match.")
+        self.assertFalse(User.objects.filter(username='wrongpass').exists())
+
+    def test_login_user_success(self):
+        User.objects.create_user(**self.credentials)
+        response = self.client.post(self.login_url, self.credentials, follow=True)
+        self.assertRedirects(response, self.home_url)
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_login_user_fail(self):
+        response = self.client.post(self.login_url, {
+            'username': 'wrong',
+            'password': 'wrongpass'
+        }, follow=True)
+        self.assertContains(response, "Incorrect username or password.")
+
+    def test_logout_user(self):
+        User.objects.create_user(**self.credentials)
+        self.client.login(**self.credentials)
+        response = self.client.post(self.logout_url, follow=True)
+        self.assertRedirects(response, self.login_url)
+        self.assertFalse(response.context['user'].is_authenticated)
